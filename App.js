92b97bed496bb4c865f7947125978e2cee60f79e
PhungCanhTuan <tuancanh252@gmail.com>
2024-09-18 14:39:31 +0700

    Tuan 6

---
+++ App.js
@@ -0,0 +1,55 @@
+//PhungCanhTuan
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+// Screens
+import HomeScreen from "./screens/Screen1";
+import DetailScreen from "./screens/Screen2";
+import { useState } from "react";
+// Screen names
+const homeName = "Home";
+const detailName = "Detail";
+const backName = "Back";
+const Tab = createBottomTabNavigator();
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let rn = route.name;
+
+          if (rn === homeName) {
+            iconName = focused ? "home" : "home-outline";
+          } else if (rn === detailName) {
+            iconName = focused ? "menu" : "list-outline";
+          } else if (rn === backName) {
+            iconName = focused ? "arrow-back" : "arrow-back-outline";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "grey",
+        labelStyle: { paddingBottom: 10, fontSize: 10 },
+        style: { padding: 10, height: 70 },
+      }}
+      initialRouteName={homeName}
+    >
+      <Tab.Screen name={detailName} component={DetailScreen} />
+      <Tab.Screen name={homeName} component={HomeScreen} />
+      <Tab.Screen name="Back" component={HomeScreen} />
+    </Tab.Navigator>
+  );
+}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
---
+++ screens/Screen2.js
@@ -0,0 +1,162 @@
+//PhungCanhTuan
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+export default function Screen2(props) {
+  const { count } = props;
+  const DATA = [
+    {
+      id: 1,
+      img: "giacchuyen1.png",
+    },
+    {
+      id: 2,
+      img: "daynguon1.png",
+    },
+    {
+      id: 3,
+      img: "dauchuyendoipsps21.png",
+    },
+    {
+      id: 4,
+      img: "dauchuyendoi1.png",
+    },
+    {
+      id: 5,
+      img: "carbusbtops21.png",
+    },
+    { id: 6, img: "daucam1.png" },
+  ];
+
+  const Item = ({ obj }) => (
+    <View style={styles.items}>
+      <View style={styles.item}>
+        <Image
+          resizeMode="contain"
+          style={styles.img}
+          source={require(`../assets/${obj.img}`)}
+        ></Image>
+        <Text>Cáp chuyển từ Cổng{"\n"} USB sang PS2...</Text>
+        <Image
+          style={{ width: 106, height: 13 }}
+          resizeMode="contain"
+          source={require("../assets/Group4.png")}
+        ></Image>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={{ fontSize: 12, fontWeight: 700 }}>69.900 đ</Text>
+          <Text
+            style={{
+              color: "rgba(150, 157, 170, 1)",
+              fontSize: 12,
+              paddingLeft: 35,
+            }}
+          >
+            -39%
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          resizeMode="contain"
+          style={{ width: 24, height: 24, float: "left" }}
+          source={require("../assets/left.png")}
+        ></Image>
+
+        <View
+          style={{
+            backgroundColor: "white",
+            borderWidth: 1,
+            width: 192,
+            height: 30,
+            flexDirection: "row",
+            padding: 5,
+          }}
+        >
+          <Image
+            style={{ width: 24, height: 24, marginLeft: 5 }}
+            source={require("../assets/search.png")}
+          ></Image>
+          <Text style={{ paddingLeft: 10 }}>Dây cáp usb</Text>
+        </View>
+        <View style={styles.shopping}>
+          <Image
+            style={{ width: 24, height: 24 }}
+            source={require("../assets/bi_cart-check.png")}
+          ></Image>
+          <Text style={styles.textShopping}>0</Text>
+        </View>
+        <AntDesign name="ellipsis1" size={24} color="white" />
+      </View>
+
+      <View style={styles.content}>
+        {/* Item 1 */}
+        <FlatList
+          data={DATA}
+          numColumns={2}
+          renderItem={({ item }) => <Item obj={item}></Item>}
+          keyExtractor={(item) => item.id}
+        ></FlatList>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "rgba(27, 169, 255, 1)",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  content: {
+    backgroundColor: "#E5E5E5",
+    paddingTop: 20,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  items: {
+    justifyContent: "space-around",
+  },
+  item: {
+    margin: 5,
+  },
+  img: {
+    width: 155,
+    height: 90,
+  },
+  shopping: {
+    flexDirection: "row",
+    position: "relative",
+    textAlign: "right",
+  },
+  textShopping: {
+    width: "17px",
+    height: "17px",
+    backgroundColor: "red",
+    borderRadius: "50%",
+    color: "#fff",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: "-5px",
+    left: "80%",
+    padding: "5px 10px",
+  },
+});
